b29052acc1d66392d7d4192f35242a5afbb365d1
Mutasem Mustafa <itsmutasem@gmail.com>
2025-10-17 15:16:29 +0300

    fix(model): dsn syntax & execute stmt

--- model.php
+++ model.php
@@ -4,13 +4,14 @@ class Model
 {
     public function getData(): array
     {
-        $dsn = 'mysql:host=localhost;dbname=project_db;charset=utf8,port=3306';
+        $dsn = 'mysql:host=localhost;dbname=product_db;charset=utf8;port=3306';
 
-        $pdo = new PDO($dsn, 'mutasem', 'muta', [
+        $pdo = new PDO($dsn, 'root', '', [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]);
 
         $stmt = $pdo->prepare("SELECT * FROM products");
+        $stmt->execute();
 
         $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
