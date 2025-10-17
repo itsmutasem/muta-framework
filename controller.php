5def1cb99748c501747c0109a5a4c3af14c60af8
Mutasem Mustafa <itsmutasem@gmail.com>
2025-10-17 15:29:15 +0300

    feat(controller): require model & view

---
+++ controller.php
@@ -0,0 +1,13 @@
+<?php
+
+class Controller
+{
+    public function index()
+    {
+        require 'model.php';
+        $model = new Model();
+        $products = $model->getData();
+
+        require 'view.php';
+    }
+}
