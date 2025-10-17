586a71685265a32a1e5c6b51c5fc148e3c350223
Mutasem Mustafa <itsmutasem@gmail.com>
2025-10-17 14:50:14 +0300

    feat(model): get data function

--- model.php
+++ model.php
@@ -2,5 +2,18 @@
 
 class Model
 {
+    public function getData(): array
+    {
+        $dsn = 'mysql:host=localhost;dbname=project_db;charset=utf8,port=3306';
 
+        $pdo = new PDO($dsn, 'mutasem', 'muta', [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+
+        $stmt = $pdo->prepare("SELECT * FROM products");
+
+        $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $products;
+    }
 }
