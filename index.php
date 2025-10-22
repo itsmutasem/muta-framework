bdaf6d2ac96df08ec99c47ae8e089e84fa54a669
Mutasem Mustafa <itsmutasem@gmail.com>
2025-10-22 17:38:52 +0300

    feat(index.php): pretty URLs
    Get the value of the URL path in the front controller & remove the query string & get the controller and action from the URL path

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 
-$action = $_GET['action'];
-$controller = $_GET['controller'];
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$segments = array_values(array_filter(explode('/', $path)));
+
+if ($segments[0] === 'muta') {
+    array_shift($segments);
+}
+
+$action = $segments[1];
+$controller = $segments[0];
 
 require "src/controllers/$controller.php";
 $controller_object = new $controller;
