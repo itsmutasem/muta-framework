8e44f884695aaacf53069dea0f21d39dff1a0696
Mutasem Mustafa <itsmutasem@gmail.com>
2025-10-22 22:24:44 +0300

    feat(product): import a class into current namespace with a use statement

--- src/App/Models/Product.php
+++ src/App/Models/Product.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use PDO;
 class Product
 {
     public function getData(): array
     {
         $dsn = 'mysql:host=localhost;dbname=product_db;charset=utf8;port=3306';
 
-        $pdo = new PDO($dsn, 'root', '', [
+        $pdo = new \PDO($dsn, 'root', '', [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]);
 
