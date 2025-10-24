b58927cf1284c8173e64c0119bdc4b7f407e08a8
Mutasem Mustafa <itsmutasem@gmail.com>
2025-10-24 14:17:36 +0300

    feat(router): route variable patterns & shorthand character classes in regular expressions

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ spl_autoload_register(function ($class_name) {
 $router = new Framework\Router;
 
 $router->add("/{controller}/{action}");
+$router->add("/{controller}/{id}/{action}");
 $router->add("/", ['controller' => 'Home', 'action' => 'index']);
 $router->add("/home/index", ['controller' => 'home', 'action' => 'index']);
 $router->add("/products", ['controller' => 'Products', 'action' => 'index']);
