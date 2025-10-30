7d6f76a8c0e002c1c521c44fe8902dd2aa5bc53e
Mutasem Mustafa <itsmutasem@gmail.com>
2025-10-30 22:45:08 +0300

    feat(dispatcher, product controller): inject the viewer into the controller

--- src/App/Controllers/Products.php
+++ src/App/Controllers/Products.php
@@ -7,21 +7,25 @@ use Framework\Viewer;
 
 class Products
 {
+    public function __construct(private Viewer $viewer)
+    {
+
+    }
+
     public function index()
     {
         $model = new Product;
         $products = $model->getData();
 
-        $viewer = new Viewer();
-        echo $viewer->render("shared/header.php");
-        echo $viewer->render("Products/index.php", ['products' => $products, 'title' => 'Products']);
+        echo $this->viewer->render("shared/header.php");
+        echo $this->viewer->render("Products/index.php", ['products' => $products, 'title' => 'Products']);
     }
 
     public function show(string $id)
     {
         $viewer = new Viewer();
-        echo $viewer->render("shared/header.php");
-        echo $viewer->render("Products/show.php", ['id' => $id,  'title' => 'Product']);
+        echo $this->viewer->render("shared/header.php");
+        echo $this->viewer->render("Products/show.php", ['id' => $id,  'title' => 'Product']);
     }
 
     public function showPage(string $title, string $id, string $page)
