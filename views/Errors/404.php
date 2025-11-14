fd4ce1305f014b507c24396824d3600b2b7723f5
Mutasem Mustafa <itsmutasem@gmail.com>
2025-11-14 16:50:13 +0300

    style(404): page

--- views/Errors/404.php
+++ views/Errors/404.php
@@ -1,13 +1,21 @@
-<!doctype html>
+<!doctype html class="h-full">
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>Page not found</title>
 </head>
 <body>
-<h1>Page not found</h1>
+<main class="grid min-h-full place-items-center bg-zinc-950 px-6 py-24 sm:py-32 lg:px-8">
+    <div class="text-center">
+<!--        <p class="text-base font-semibold text-violet-600">404</p>-->
+        <h1 class="mt-4 text-5xl font-semibold tracking-tight text-balance text-white sm:text-6xl"><span class="text-violet-600">404</span> | Page not found</h1>
+        <p class="mt-6 text-lg font-medium text-pretty text-white/80 sm:text-xl/8">Sorry, we couldn’t find the page you’re looking for.</p>
+    </div>
+</main>
+
 </body>
 </html>
