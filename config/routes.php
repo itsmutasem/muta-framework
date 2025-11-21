0450c0f5fc44e0ac487da81908ec8c38a2a0a71f
Mutasem Mustafa <itsmutasem@gmail.com>
2025-11-21 16:35:30 +0300

    refactor(routes): replace the general route with an id with specific routes

--- config/routes.php
+++ config/routes.php
@@ -5,7 +5,13 @@ $router = new Framework\Router;
 $router->add("/admin/{controller}/{action}", ['namespace' => "Admin"]);
 $router->add("/{title}/{id:\d+}/{page:\d+}", ['controller' => 'products', 'action' => 'showPage']);
 $router->add("/product/{slug:[\w-]+}", ['controller' => 'Products', 'action' => 'show']);
-$router->add("/{controller}/{id:\d+}/{action}");
+//$router->add("/{controller}/{id:\d+}/{action}");
+$router->add("{controller}/{id:\d+}/show", ['action' => 'show']);
+$router->add("{controller}/{id:\d+}/edit", ['action' => 'edit']);
+$router->add("{controller}/{id:\d+}/update", ['action' => 'update']);
+$router->add("{controller}/{id:\d+}/delete", ['action' => 'delete']);
+$router->add("{controller}/{id:\d+}/destroy", ['action' => 'destroy']);
+
 $router->add("/", ['controller' => 'Home', 'action' => 'index']);
 $router->add("/home/index", ['controller' => 'home', 'action' => 'index']);
 $router->add("/products", ['controller' => 'Products', 'action' => 'index']);
