a7b431e67bea687f62469476ff0c92374f1a5107
Mutasem Mustafa <itsmutasem@gmail.com>
2025-11-21 22:44:51 +0300

    refactor(product view/index): use {{ }} for print the variables

--- views/Products/index.mvc.php
+++ views/Products/index.mvc.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
-    <title><?= $title ?></title>
+    <title>{{ title }}</title>
 </head>
 <body>
 <h1>Product List</h1>
@@ -12,8 +12,8 @@
 <?php foreach ($products as $product) : ?>
     <div>
         <h2>
-            <a href="/products/<?= $product['id'] ?>/show">
-                <?= htmlspecialchars($product['name']) ?>
+            <a href="/products/{{ product['id'] }}/show">
+                {{ product['name'] }}
             </a>
         </h2>
     </div>
