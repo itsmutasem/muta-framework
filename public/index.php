8ecafc37551dbb71b1595963f64908605b68bf66
Mutasem Mustafa <itsmutasem@gmail.com>
2025-11-21 17:22:55 +0300

    feat(request, index): add other superglobals  to the request class

--- public/index.php
+++ public/index.php
@@ -20,6 +20,6 @@ $container = require ROOT_PATH . "/config/services.php";
 
 $dispatcher = new Framework\Dispatcher($router, $container);
 
-$request = new Framework\Request($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+$request = Framework\Request::createFromGlobals();
 
 $dispatcher->handle($request);
