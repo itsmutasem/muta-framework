90933800a48b6df90abf2e153a314cfd47762603
Mutasem Mustafa <itsmutasem@gmail.com>
2025-11-24 22:18:29 +0300

    feat(change response example middleware): add a middleware class that modifies the response

--- src/App/Middleware/ChangeResponseExample.php
+++ src/App/Middleware/ChangeResponseExample.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 
 namespace App\Middleware;
 
+use Framework\Request;
+use Framework\Response;
+
 class ChangeResponseExample
 {
-
+    public function process(Request $request, $next): Response
+    {
+        $response = $next->hadle($request);
+        $response->setBody($response->getBody() . " - Middleware");
+        return $response;
+    }
 }
