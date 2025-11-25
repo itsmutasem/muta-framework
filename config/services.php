2eaac68875b23f1c041665bd5c0e9413746b1967
Mutasem Mustafa <itsmutasem@gmail.com>
2025-11-25 22:43:23 +0300

    feat(services): Inject CsrfToken into MVCTemplateViewer

--- config/services.php
+++ config/services.php
@@ -4,7 +4,10 @@ $container = new Framework\Container;
 $container->set(App\Database::class, function () {
     return new App\Database($_ENV["DB_HOST"], $_ENV["DB_NAME"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"]);
 });
-$container->set(Framework\TemplateViewerInterface::class, function () {
-    return new Framework\MVCTemplateViewer();
+$container->set(Framework\Security\CsrfToken::class, function () {
+    return new Framework\Security\CsrfToken();
+});
+$container->set(Framework\TemplateViewerInterface::class, function () use ($container) {
+    return new Framework\MVCTemplateViewer($container->get(Framework\Security\CsrfToken::class));
 });
 return $container;
