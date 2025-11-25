838c6ecdcf24ee70222d67e4aeafb20cbbb0905d
Mutasem Mustafa <itsmutasem@gmail.com>
2025-11-25 20:19:56 +0300

    feat(sanitizer): check if the data is array and recursively sanitize each value

--- src/Framework/Security/Sanitizer.php
+++ src/Framework/Security/Sanitizer.php
@@ -1,8 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Framework\Security;
 
 class Sanitizer
 {
-    
+    public static function clean(mixed $data): mixed
+    {
+        if (is_array($data)) {
+            foreach ($data as $key => $value) {
+                $data[$key] = self::clean($value);
+            }
+            return $data;
+        }
+    }
 }
