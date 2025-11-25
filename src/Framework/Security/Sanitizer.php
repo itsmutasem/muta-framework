4b371a91326eda2a46cf065b1ae93256604a6905
Mutasem Mustafa <itsmutasem@gmail.com>
2025-11-25 20:28:15 +0300

    security(sanitizer): strip dangerous html tags

--- src/Framework/Security/Sanitizer.php
+++ src/Framework/Security/Sanitizer.php
@@ -21,5 +21,6 @@ class Sanitizer
 
         $data = preg_replace('/[\x00-\x1F\x7f]/u', '', $data);
         $data = preg_replace('#<(secript|style)[^>]*>.*?</\1>#is', '', $data);
+        $data = strip_tags($data);
     }
 }
