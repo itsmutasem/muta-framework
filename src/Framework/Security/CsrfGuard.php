6316f3abc0554b1b2678da43e49181753ec0aa17
Mutasem Mustafa <itsmutasem@gmail.com>
2025-11-25 22:23:40 +0300

    feat(csrf guard): implement csrf guard as middleware

--- src/Framework/Security/CsrfGuard.php
+++ src/Framework/Security/CsrfGuard.php
@@ -4,7 +4,29 @@ declare(strict_types=1);
 
 namespace Framework\Security;
 
-class CsrfGuard
-{
+use Framework\MiddlewareInterface;
+use Framework\Request;
+use Framework\RequestHandlerInterface;
+use Framework\Response;
 
+class CsrfGuard implements MiddlewareInterface
+{
+    public function __construct(private CsrfToken $csrfToken)
+    {
+    }
+
+    public function process(Request $request, RequestHandlerInterface $next): Response
+    {
+        $method = $request->method;
+        if (in_array($method, ['POST', 'PUT', 'DELETE', 'PATCH'])) {
+            $token = $request->post['csrf_token'] ?? $request->server['HTTP_X_CSRF_TOKEN'] ?? null;
+            if (!$this->csrfToken->verify($token)) {
+                $response = new Response();
+                $response->setBody('CSRF Token in invalid');
+                $response->setStatusCode(403);
+                return $response;
+            }
+        }
+        return $next->handle($request);
+    }
 }
