9bcdbbc300654b6d29a3d89ef28a4ee93a2739df
Mutasem Mustafa <itsmutasem@gmail.com>
2025-11-25 22:14:25 +0300

    security(csrf token): generate token

--- src/Framework/Security/CsrfToken.php
+++ src/Framework/Security/CsrfToken.php
@@ -6,5 +6,11 @@ namespace Framework\Security;
 
 class CsrfToken
 {
-
+    public function generate(): string
+    {
+        if (empty($_SESSION['csrf_token'])) {
+            $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+        }
+        return $_SESSION['csrf_token'];
+    }
 }
