f6b626b691c950fac88bd3f46d050ba2d3348efa
Mutasem Mustafa <itsmutasem@gmail.com>
2025-11-26 16:46:11 +0300

    feat(form): display the validation errors

--- views/Products/form.mvc.php
+++ views/Products/form.mvc.php
@@ -1,11 +1,15 @@
-<label for="name">Name</label>
-<input type="text" name="name" id="name" value="{{ product['name'] }}">
 <input type="hidden" name="csrf_token" value="{{ csrf_token }}">
 
-<< if(isset($errors['name'])) : >>
-    <p>{{ errors['name'] }}</p>
+<label for="name">Name</label>
+<input type="text" name="name" id="name" value="{{ product['name'] }}">
+<< if(!empty($errors['name'][0])) : >>
+    <p>{{ errors['name'][0] }}</p>
 << endif; >>
 
 <label for="description">Description</label>
 <textarea name="description" id="description" cols="30" rows="10">{{ product['description'] }}</textarea>
+<< if(!empty($errors['description'][0])) : >>
+<p>{{ errors['description'][0] }}</p>
+<< endif; >>
+
 <button type="submit">Save</button>
