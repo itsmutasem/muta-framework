3da23b909a61ad07c77285544ae064d122ae1358
Mutasem Mustafa <itsmutasem@gmail.com>
2025-11-26 14:05:11 +0300

    feat(validator): define protected variabels and the cunstruct

--- src/Framework/Validator.php
+++ src/Framework/Validator.php
@@ -4,5 +4,14 @@ namespace Framework;
 
 class Validator
 {
+    protected  array $data = [];
+    protected array $rules;
+    protected array $errors = [];
+    protected array $validated = [];
 
+    public function __construct(array $data, array $rules)
+    {
+        $this->data = $data;
+        $this->rules = $rules;
+    }
 }
