70a2aedd9f37e069c8908eb0c5f3689090aba2e7
Mutasem Mustafa <itsmutasem@gmail.com>
2025-12-16 00:31:35 +0300

    feat(command): info method

--- src/Framework/Console/Command.php
+++ src/Framework/Console/Command.php
@@ -4,4 +4,12 @@ namespace Framework\Console;
 
 abstract class Command
 {
+    abstract public function signature(): string;
+    abstract public function description(): string;
+    abstract public function handle(array $args): void;
+
+    protected function info(string $message): void
+    {
+        echo "\033[32m✔ {$message}\033[0m\n";
+    }
 }
