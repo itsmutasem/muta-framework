80c3380b826bada90eb2729c1b2fa843c03a60fa
Mutasem Mustafa <itsmutasem@gmail.com>
2025-12-17 18:56:48 +0300

    feat(make controller): handle commands for action

--- src/Framework/Console/Commands/MakeController.php
+++ src/Framework/Console/Commands/MakeController.php
@@ -19,6 +19,38 @@ class MakeController extends Command
 
     public function handle(array $args): void
     {
+        $name = $args[0] ?? '';
 
+        if (!$name) {
+            $this->error('Please provide a controller name');
+            return;
+        }
+
+        $parts = explode('/', $name);
+        $class = array_pop($parts);
+        $subPath = implode('/', $parts);
+
+        $basePath = 'src/App/Controllers';
+        $path = $basePath . ($subPath ? "/{$subPath}" : '');
+        $file = "{$path}/{$class}.php";
+
+        if (!is_dir($path)) {
+            mkdir($path, 0777, true);
+        }
+
+        if (file_exists($file)) {
+            $this->error("Controller '{$class}' already exists");
+            return;
+        }
+
+        $namespace = 'App\Controllers' . ($subPath ? '\\' . str_replace('/', '\\', $subPath) : '');
+        $stubPath = dirname(__DIR__, 4) . '/stubs/controller.stub';
+        if (!file_exists($stubPath)) {
+            $this->error("Stub file '{$stubPath}' not found");
+        }
+        $stub = file_get_contents($stubPath);
+        $content = str_replace(['{{namespace}}', '{{class}}'], [$namespace, $class], $stub);
+        file_put_contents($file, $content);
+        $this->info("Controller '{$class}' created successfully");
     }
 }
