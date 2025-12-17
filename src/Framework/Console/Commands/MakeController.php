d5335b26d8b8a4f34033bd808ba7055b92520695
Mutasem Mustafa <itsmutasem@gmail.com>
2025-12-17 17:06:04 +0300

    Create MakeController.php

---
+++ src/Framework/Console/Commands/MakeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Framework\Console\Commands;
+
+use Framework\Console\Command;
+
+class MakeController extends Command
+{
+
+    public function signature(): string
+    {
+        
+    }
+
+    public function description(): string
+    {
+
+    }
+
+    public function handle(array $args): void
+    {
+
+    }
+}
