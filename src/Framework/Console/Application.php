df629886377dd6c8384240ef6989ad0ef09c5786
Mutasem Mustafa <itsmutasem@gmail.com>
2025-12-17 16:35:13 +0300

    feat(application): register method

--- src/Framework/Console/Application.php
+++ src/Framework/Console/Application.php
@@ -4,4 +4,15 @@ namespace Framework\Console;
 
 class Application
 {
+    protected array $commands = [];
+
+    public function __construct()
+    {
+
+    }
+
+    public function register(Command $command): void
+    {
+        $this->commands[$command->signature()] = $command;
+    }
 }
