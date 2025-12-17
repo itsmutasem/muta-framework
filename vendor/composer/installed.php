a7a482c93cf1dc21c233207abad0c9524c451403
Mutasem Mustafa <itsmutasem@gmail.com>
2025-12-17 18:55:23 +0300

    feat: install composer vendor autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mutasem/muta-framework',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1869fede6ce8ed2169a4b4e8d6707455affb4394',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mutasem/muta-framework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1869fede6ce8ed2169a4b4e8d6707455affb4394',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
