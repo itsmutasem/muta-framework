0d4c35fc232774ac7393f6b85762fb6503f94d44
Mutasem Mustafa <itsmutasem@gmail.com>
2025-12-22 22:13:33 +0300

    feat(command): add a success method for success operations

--- src/Framework/Console/Command.php
+++ src/Framework/Console/Command.php
@@ -17,4 +17,9 @@ abstract class Command
     {
         echo "\033[31m✖ {$message}\033[0m\n";
     }
+
+    protected function success(string $message): void
+    {
+        echo "\033[32m✔ {$message}\033[0m\n";
+    }
 }
