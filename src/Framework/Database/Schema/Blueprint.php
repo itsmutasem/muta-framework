8f1e2f8c1a8ef3961a0013cc8a3a1e7c730dd40e
Mutasem Mustafa <itsmutasem@gmail.com>
2025-12-25 21:14:42 +0300

    feat(database blue print): define the public variables and construct

--- src/Framework/Database/Schema/Blueprint.php
+++ src/Framework/Database/Schema/Blueprint.php
@@ -3,5 +3,12 @@
 namespace Framework\Database\Schema;
 class Blueprint
 {
+    public string $table;
+    public array $columns = [];
+    public array $commands = [];
 
+    public function __construct(string $table)
+    {
+        $this->table = $table;
+    }
 }
