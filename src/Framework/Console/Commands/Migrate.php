7f967ec385fe2af30ec81c2d074b299220623781
Mutasem Mustafa <itsmutasem@gmail.com>
2025-12-25 23:40:32 +0300

    feat(migrate): ensure migrations tables

--- src/Framework/Console/Commands/Migrate.php
+++ src/Framework/Console/Commands/Migrate.php
@@ -3,6 +3,7 @@
 namespace Framework\Console\Commands;
 
 use Framework\Console\Command;
+use PDO;
 
 class Migrate extends Command
 {
@@ -20,5 +21,15 @@ class Migrate extends Command
     public function handle(array $args): void
     {
         // TODO: Implement handle() method.
+    protected function ensureMigrationsTable(PDO $db): void
+    {
+        $db->exec("
+        CREATE TABLE IF NOT EXISTS migrations (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                migration VARCHAR(255) UNIQUE,
+                batch INT,
+                executed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        ");
     }
 }
