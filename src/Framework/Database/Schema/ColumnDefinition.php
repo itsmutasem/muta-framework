fcac74ecb17adc5127199e286ab133c6a648fb90
Mutasem Mustafa <itsmutasem@gmail.com>
2025-12-25 21:20:57 +0300

    feat(database column definition): define the public variables and the construct

--- src/Framework/Database/Schema/ColumnDefinition.php
+++ src/Framework/Database/Schema/ColumnDefinition.php
@@ -4,5 +4,18 @@ namespace Framework\Database\Schema;
 
 class ColumnDefinition
 {
+    public string $type;
+    public string $name;
+    public array $params;
+    public bool $nullable = false;
+    public bool $unique = false;
+    public bool $primary = false;
+
+    public function __construct(string $type, string $name, array $params = [])
+    {
+        $this->type = $type;
+        $this->name = $name;
+        $this->params = $params;
+    }
 
 }
