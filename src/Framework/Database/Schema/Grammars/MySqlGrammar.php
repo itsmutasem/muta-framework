83115c7922c356f67bc192ce391f56684b8a57d0
Mutasem Mustafa <itsmutasem@gmail.com>
2025-12-25 22:15:04 +0300

    feat(my sql grammar): compile create

--- src/Framework/Database/Schema/Grammars/MySqlGrammar.php
+++ src/Framework/Database/Schema/Grammars/MySqlGrammar.php
@@ -2,7 +2,20 @@
 
 namespace Framework\Database\Schema\Grammars;
 
+use Framework\Database\Schema\Blueprint;
+
 class MySqlGrammar
 {
-
+    public function compileCreate(Blueprint $blueprint): string
+    {
+        $columns = [];
+        foreach ($blueprint->columns as $column) {
+            $columns[] = $this->compileCreate($column);
+        }
+        return sprintf(
+            "CREATE TABLE %s (%s)",
+            $blueprint->table,
+            implode(', ', $columns)
+        );
+    }
 }
