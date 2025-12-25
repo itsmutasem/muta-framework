45616ce1d3bf521ec95a85004007dc147b917ceb
Mutasem Mustafa <itsmutasem@gmail.com>
2025-12-25 22:31:55 +0300

    feat(database schema): create object for blueprint and my sql grammar and compile it

--- src/Framework/Database/Schema/Schema.php
+++ src/Framework/Database/Schema/Schema.php
@@ -2,7 +2,17 @@
 
 namespace Framework\Database\Schema;
 
+use Framework\Database\Schema\Grammars\MySqlGrammar;
+use PDO;
+
 class Schema
 {
-
+    public static function create(string $table, callable $callback): void
+    {
+        $blueprint = new Blueprint($table);
+        $callback($blueprint);
+        $grammar = new MySqlGrammar();
+        $sql = $grammar->compileCreate($blueprint);
+        app(PDO::class)->exec($sql);
+    }
 }
