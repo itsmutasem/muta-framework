a49c3925126e7c4a154c3c797f711e5b252c01cd
Mutasem Mustafa <itsmutasem@gmail.com>
2025-12-27 17:07:22 +0300

    Create MakeRateLimit.php

---
+++ src/Framework/Console/Commands/MakeRateLimit.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Framework\Console\Commands;
+
+use Framework\Console\Command;
+
+class MakeRateLimit extends Command
+{
+
+    public function signature(): string
+    {
+        // TODO: Implement signature() method.
+    }
+
+    public function description(): string
+    {
+        // TODO: Implement description() method.
+    }
+
+    public function handle(array $args): void
+    {
+        // TODO: Implement handle() method.
+    }
+}
